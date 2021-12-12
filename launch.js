313cb9f035fcf36f40e25c76cab1862a6e9f1cda
lala <95991924+lalaBCpurple@users.noreply.github.com>
2021-12-11 21:34:51 -0800

    Add files via upload

--- launch.js
+++ launch.js
@@ -1,9 +1,2 @@
 // javascript:
-(function(){
-  const js="https://lalabcpurple.github.io/chatbox/chatbox.js";
-  let s=document.createElement("script");
-  s.setAttribute("language","JavaScript");
-  s.setAttribute("crossorigin","anonymous");
-  s.setAttribute("src",js);
-  s.onload=()=>s.remove();
-  document.head.appendChild(s);})();
+(function(){const js="https://lalabcpurple.github.io/chatbox/chatbox.js";let s=document.createElement("script");s.setAttribute("language","JavaScript");s.setAttribute("crossorigin","anonymous");s.setAttribute("src",js);s.onload=()=>s.remove();document.head.appendChild(s);})();
