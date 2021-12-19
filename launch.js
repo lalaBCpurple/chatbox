d3d0c54dbbce7e7d234e1d0ef0e6d12864b643a3
lala <95991924+lalaBCpurple@users.noreply.github.com>
2021-12-18 18:47:49 -0800

    Delete launch.js

--- launch.js
+++
@@ -1,2 +0,0 @@
-// javascript:
-(function(){const js="https://lalabcpurple.github.io/chatbox/chatbox.js";let s=document.createElement("script");s.setAttribute("language","JavaScript");s.setAttribute("crossorigin","anonymous");s.setAttribute("src",js);s.onload=()=>s.remove();document.head.appendChild(s);})();
