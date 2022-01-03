7cf43a4b9a75b57aecc433508ca62688aae45bc1
lala <95991924+lalaBCpurple@users.noreply.github.com>
2022-01-02 16:49:17 -0800

    Add files via upload

---
+++ loader.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Chatbox (Loader)
+// @namespace    Chatbox
+// @version      0.1
+// @description  Loader for lala's Chatbox
+// @author       lalaBCpurple
+// @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
+// @homepage     https://lalabcpurple.github.io/chatbox/chatbox.html
+// @source       https://github.com/lalaBCpurple/chatbox
+// @downloadURL  https://lalabcpurple.github.io/chatbox/loader.js
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+setTimeout(
+	function () {
+		if (window.BCX_Loaded === undefined) {
+			let n = document.createElement("script");
+			n.setAttribute("language", "JavaScript");
+			n.setAttribute("crossorigin", "anonymous");
+			n.setAttribute("src", "https://lalabcpurple.github.io/chatbox/chatbox.js?_=" + Date.now());
+			n.onload = () => n.remove();
+			document.head.appendChild(n);
+		}
+	},
+	2000
+);
